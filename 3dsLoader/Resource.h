821eb3687bac2ab7923d0bd765facdef2bf81119
cxcscut <393171302@qq.com>
2017-09-02 20:58:54 +0800

    uploading

---
+++ 3dsLoader/Resource.h
@@ -0,0 +1,44 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 3DSLoader.rc 使用
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_3DSLOATYPE                  129
+#define IDR_MENU1                       132
+#define IDD_DIALOG1                     133
+#define IDD_DIALOG_RobotControl         133
+#define ID_Reset                        32771
+#define ID_Thumb                        32772
+#define ID_Thumb_Inverse                32773
+#define ID_Index                        32774
+#define ID_Index_Inverse                32775
+#define ID_Middle                       32776
+#define ID_Middle_Inverse               32777
+#define ID_Ring                         32778
+#define ID_Ring_Inverse                 32779
+#define ID_Little                       32780
+#define ID_Little_Inverse               32781
+#define ID_grab                         32782
+#define ID_Fist                         32783
+#define ID_Push                         32784
+#define ID_Clamp                        32770
+#define ID_Pull                        32769
+#define ID_Insert                        32768
+#define ID_Support                         32767
+#define ID_Squeeze                        32766
+#define ID_Cut                        32765
+#define ID_Rip                        32764
+#define ID_Tongs                        32763
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        135
+#define _APS_NEXT_COMMAND_VALUE         32779
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
